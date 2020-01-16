9b303aad3b49163a71f4d4144e0ff4b30e41226a
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-16 10:30:09 +0700

    [8.0][MIG] hr_attendance_geolocation Backport 12.0 to 8.0

--- hr_attendance_geolocation/models/hr_attendance.py
+++ hr_attendance_geolocation/models/hr_attendance.py
@@ -1,8 +1,27 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019 Eficent Business and IT Consulting Services S.L..
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-from odoo import fields, models
-from odoo.addons import decimal_precision as dp
+from openerp import fields, models
+import openerp.addons.decimal_precision as dp
 
 UNIT = dp.get_precision("Location")
 
@@ -10,23 +29,13 @@ UNIT = dp.get_precision("Location")
 class HrAttendance(models.Model):
     _inherit = "hr.attendance"
 
-    check_in_latitude = fields.Float(
-        "Check-in Latitude",
+    latitude = fields.Float(
+        string="Latitude",
         digits=UNIT,
         readonly=True
     )
-    check_in_longitude = fields.Float(
-        "Check-in Longitude",
-        digits=UNIT,
-        readonly=True
-    )
-    check_out_latitude = fields.Float(
-        "Check-out Latitude",
-        digits=UNIT,
-        readonly=True
-    )
-    check_out_longitude = fields.Float(
-        "Check-out Longitude",
+    longitude = fields.Float(
+        string="Longitude",
         digits=UNIT,
         readonly=True
     )
